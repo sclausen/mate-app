16d15644f82aa307fb940431d91fa4c6f8e05ea3
Sebastian Clausen <s.clausen@greylogix.de>
2014-05-06 09:53:02 +0200

    wait for translations

--- client/routers/config.js
+++ client/routers/config.js
@@ -6,6 +6,9 @@ function navigatorLanguage() {
 Router.configure({
   layoutTemplate: 'layout',
   notFoundTemplate: 'not_found',
+  waitOn: function() {
+    return Meteor.subscribe("translations");
+  },
   onBeforeAction: function() {
     Session.set("maxSize", 7);
     Session.set("documentsPerPage", 10);
@@ -13,6 +16,17 @@ Router.configure({
     var language = this.params.language || navigatorLanguage();
     if (language && Meteor.getLocale() !== language) {
       Meteor.setLocale(language);
+    }
+
+    if (this.ready()) {
+      translations = {};
+      Translations.find().map(function(tx) {
+        translations[tx.key] = {};
+        _.each(_.omit(Object.keys(tx), "_id", "key"), function(key) {
+          translations[tx.key][key] = tx[key];
+        });
+      });
+      Meteor.i18nMessages = translations;
     }
   }
 });
